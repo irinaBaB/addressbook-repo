405f15176522fb22007e4cd905fb53f5b998411f
Irina Pirandello <irinapirandello@irinas-mbp-2.lan>
2023-06-07 21:17:19 +1000

    first ex done

--- test_one.py
+++ test_one.py
@@ -1,11 +1,6 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
 
 
 class UntitledTestCase(unittest.TestCase):
@@ -37,19 +32,6 @@ class UntitledTestCase(unittest.TestCase):
         wd.find_element_by_link_text("groups").click()
         wd.find_element_by_link_text("Logout").click()
 
-    # def is_element_present(self, how, what):
-    #     try:
-    #         self.driver.find_element(by=how, value=what)
-    #     except NoSuchElementException as e:
-    #         return False
-    #     return True
-    #
-    # def is_alert_present(self):
-    #     try:
-    #         self.driver.switch_to_alert()
-    #     except NoAlertPresentException as e:
-    #         return False
-    #     return True
 
     def tearDown(self):
         self.wd.quit()
