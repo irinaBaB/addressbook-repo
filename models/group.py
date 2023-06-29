d1fd8d695b74d9abe3ed392c8021ebd7c2ef8cde
Irina Pirandello <irinapirandello@irinas-mbp-2.lan>
2023-06-29 23:43:54 +1000

    adding eq function to validate logical values of the lists

--- models/group.py
+++ models/group.py
@@ -5,3 +5,11 @@ class Group:
         self.header = header
         self.footer = footer
         self.idg = idg
+
+    def __repr__(self):
+       return "%s:%s" % (self.idg, self.name)
+
+    def __eq__(self, other):
+        return self.idg == other.idg and self.name == other.name
+
+
