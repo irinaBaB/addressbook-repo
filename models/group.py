a54f0fa683856f19013b62a45deb8cd8bfe5415c
Irina Pirandello <irinapirandello@irinas-mbp-2.lan>
2023-07-02 22:16:04 +1000

    adding new verifications for create test checking the values of the list

--- models/group.py
+++ models/group.py
@@ -1,4 +1,4 @@
-
+from sys import maxsize
 class Group:
     def __init__(self, name=None, header=None, footer=None, idg=None):
         self.name = name
@@ -10,6 +10,12 @@ class Group:
        return "%s:%s" % (self.idg, self.name)
 
     def __eq__(self, other):
-        return self.idg == other.idg and self.name == other.name
+        return (self.idg is None or other.idg is None or self.idg == other.idg) and self.name == other.name
+
+    def id_or_max(self):
+        if self.idg:
+            return int(self.idg)
+        else:
+            return maxsize
 
 
--- tests/test_create_group.py
+++ tests/test_create_group.py
@@ -1,14 +1,23 @@
 from models.group import Group
 
 
+
 def test_case_create_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="test1", header="one test", footer="second field"))
+    group = Group(name="test1", header="one test", footer="second field")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups)+1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
+
 
 def test_case_create_group_empty_fields(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="", header="", footer=""))
+    group = Group(name="test1", header="one test", footer="second field")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups)+1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
