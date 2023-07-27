67d8d57461f1586e5cc0c1115b680fc10e586cda
irinaBaB <44655446+irinaBaB@users.noreply.github.com>
2023-07-28 09:32:21 +1000

    email spcechial charachters fix

--- tests/test_phones.py
+++ tests/test_phones.py
@@ -2,12 +2,12 @@ import re
 from models.contact import Contact
 
 def test_phones_and_contacts_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_info_from_edit_page(0)
+    contact_from_home_page = app.contact.get_contact_list()[4]
+    contact_from_edit_page = app.contact.get_info_from_edit_page(4)
     assert contact_from_home_page.firstname == contact_from_edit_page.firstname
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname
     assert contact_from_home_page.address == contact_from_edit_page.address
-    assert contact_from_home_page.email == merge_emails_to_verify_with_home_page(contact_from_edit_page)
+    assert clear(contact_from_home_page.email) == merge_emails_to_verify_with_home_page(contact_from_edit_page)
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
 
 
